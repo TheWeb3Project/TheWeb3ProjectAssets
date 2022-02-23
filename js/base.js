1a41f196d6f6b357be849e89dfb9865ede6d8c26
upfinity-main <97693128+upfinity-main@users.noreply.github.com>
2022-02-24 00:31:48 +0900

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,3 +1,13 @@
+BNBDIV = 10**18;
+
+function round(value, n) {
+  return value.toFixed(n);
+}
+function BNB(value, n=4) {
+  value = parseInt(value);
+  return round(value / BNBDIV, n);
+}
+
 function select(el, all = true) {
   el = el.trim()
   if (all) {
